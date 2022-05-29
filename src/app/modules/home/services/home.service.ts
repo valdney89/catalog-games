52a0aea553082e5cbbf849833bf6e9fae38a1b6c
Valdney Nogueira <valdneysouza@gmail.com>
2022-05-29 19:42:40 -0300

    feat: creating service to get consoles and changes in catalog

--- src/app/modules/home/services/home.service.ts
+++ src/app/modules/home/services/home.service.ts
@@ -1,23 +1,44 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map, Observable, Subject } from 'rxjs';
 
-import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
-import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Catalog } from '../models/catalog';
+import { Console } from '../models/console';
 
-import { environment } from "src/environments/environment";
-import { Catalog } from "../models/catalog";
-
-const APIURL = `${environment.apiUrl}catalog`
+const APIURL_CATALOG = `${environment.apiUrl}catalog?_sort=dateCompletion&_order=desc`;
+const APIURL_CONSOLES = `${environment.apiUrl}consoles`;
 
 @Injectable({ providedIn: 'root' })
 export class HomeService {
+  catalogsSource = new Subject<void>();
+  currentCatalogs: Observable<void> = this.catalogsSource.asObservable();
 
-  constructor(private http: HttpClient){}
+  constructor(private http: HttpClient) {}
 
-  getCatalog(): Observable<Catalog[]>{
-    return this.http.get<Catalog[]>(APIURL);
+  changeCatalogs(){
+    this.catalogsSource.next();
+  }
+
+  getCatalog(): Observable<Catalog[]> {
+    return this.http.get<Catalog[]>(APIURL_CATALOG);
+  }
+
+  getCatalogByName(value: string): Observable<Catalog[]> {
+    return this.http.get<Catalog[]>(APIURL_CATALOG).pipe(
+      map((data) => {
+        return data.filter((catalog) =>
+          catalog.title.toLowerCase().includes(value?.toLowerCase())
+        );
+      })
+    );
   }
 
   addCatalog(catalog: Catalog): Observable<Catalog> {
-    return this.http.post<Catalog>(APIURL, catalog)
+    return this.http.post<Catalog>(APIURL_CATALOG, catalog);
+  }
+
+  getConsoles(): Observable<Console[]> {
+    return this.http.get<Console[]>(APIURL_CONSOLES);
   }
 }
